a459fd94cd2eb2cbb5a55ca6de51db3736cc27e7
sahil <sahsdep@example.com>
2024-03-02 15:12:38 +0530

    I am able to extract skills after hardcoding them

--- file.py
+++ file.py
@@ -1,68 +1,184 @@
-import PyPDF2
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from tika import parser
+file = r'Resume_Sahil_Deshpande.pdf'
+file_data = parser.from_file(file)
+text = file_data['content']
+# print(text)
+
+
+#E-MAIL
 import re
-def extract_text_from_pdf(pdf_file):
-    text = ""
-    with open(pdf_file, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        num_pages = len(pdf_reader.pages)
-        for page_num in range(num_pages):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
-    return text
-# not working
-def remove_extra_spaces(text):
-    # Replace multiple spaces with a single space
-    text = re.sub(r'\s+', ' ', text)
-    # Remove unnecessary spaces after specific characters like ':', ',', etc.
-    text = re.sub(r'\s*([:;,])\s*', r'\1 ', text)
-    return text.strip()  
-def parse_resume(text):
-    # Dummy parsing logic for demonstration
-    # name_pattern = r"Name: (.+)"
-    email_pattern = r"Email  : (.+)"
-    # print(f"email_pattern = {email_pattern}")
-    phone_pattern = r"Mobile  : (.+)"
-    # print(f"phone_pattern = {phone_pattern}")
-    education_pattern = r"College : (.+)"
-    # experience_pattern = r"Experience: (.+)"
-    
-    # name_match = re.search(name_pattern, text)
-    email_match = re.search(email_pattern, text)
-    # print(f"email_match = {email_match}")
-    phone_match = re.search(phone_pattern, text)
-    # print(f"phone_match = {phone_match}")
-    education_match = re.search(education_pattern, text)
-    # print(f"education_match = {education_match}")
-    # experience_match = re.search(experience_pattern, text)
-    
-    # name = name_match.group(1) if name_match else ""
-    email = email_match.group(1) if email_match else ""
-    # print(f"email = {email}")
-    phone = phone_match.group(1) if phone_match else ""
-    # print(f"phone = {phone}")
-    education = education_match.group(1) if education_match else ""
-    # print(f"education = {education}")
-    # experience = experience_match.group(1) if experience_match else ""
-    
-    return  email, phone, education
+def get_email_addresses(string):
+    r = re.compile(r'[\w\.-]+@[\w\.-]+')
+    return r.findall(string)
+extracted_text= {}
+email = get_email_addresses(text)
+print(email)
+
+extracted_text["E-Mail"] = email
+
+def get_phone_numbers(string):
+    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    phone_numbers = r.findall(string)
+    return [re.sub(r'\D', '', num) for num in phone_numbers]
+
+phone_number= get_phone_numbers(text)
+print(phone_number)
+
+extracted_text["Phone Number"] = phone_number
 
 
-def write_to_csv(data, csv_file):
-    import csv
-    with open(csv_file, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Email", "Phone", "Education"])
-        writer.writerow(data)
 
-if __name__ == "__main__":
-    pdf_file = "Resume_Sahil_Deshpande.pdf"  # Change this to your PDF file path
-    csv_file = "resume_info.csv"  # Change this to your desired CSV file path
+#Name
+import spacy
+from spacy.matcher import Matcher
+
+# load pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+# initialize matcher with a vocab
+matcher = Matcher(nlp.vocab)
+
+# def extract_name(resume_text):
+#     nlp_text = nlp(resume_text)
     
-    text = extract_text_from_pdf(pdf_file)
-    # print(text)
-    # print("***************************")
-    # text = remove_extra_spaces(text)
-    print(text)
-    email, phone, education = parse_resume(text)
-    data = [ email, phone, education]
-    write_to_csv(data, csv_file)
+#     # First name and Last name are always Proper Nouns
+#     pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+    
+#     matcher.add('NAME', [pattern], on_match = None)
+    
+#     matches = matcher(nlp_text)
+    
+#     for match_id, start, end in matches:
+#         span = nlp_text[start:end]
+#         return span.text
+    
+    
+# name = extract_name(text)
+# print(name)
+# extracted_text["Name"] = name
+
+
+
+
+import spacy
+
+# load pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+
+def extract_skills(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # removing stop words and implementing word tokenization
+    tokens = [token.text for token in nlp_text if not token.is_stop]
+    
+    skills = ["machine learning",
+             "deep learning",
+             "nlp",
+             "natural language processing",
+             "mysql",
+             "sql",
+             "django",
+             "computer vision",
+              "tensorflow",
+             "opencv",
+             "mongodb",
+             "artificial intelligence",
+             "ai",
+             "flask",
+             "robotics",
+             "data structures",
+             "python",
+             "c++",
+             "matlab",
+             "css",
+             "html",
+             "github",
+             "php"]
+    
+    skillset = []
+    
+    # check for one-grams (example: python)
+    for token in tokens:
+        if token.lower() in skills:
+            skillset.append(token)
+    
+    # check for bi-grams and tri-grams (example: machine learning)
+    for token in nlp_text.noun_chunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skillset.append(token)
+    
+    return [i.capitalize() for i in set([i.lower() for i in skillset])]
+
+skills = []
+skills = extract_skills(text)
+
+extracted_text["Skills"] = skills
+print(skills)
+
+
+
+import re
+import spacy
+from nltk.corpus import stopwords
+
+# load pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+# Grad all general stop words
+STOPWORDS = set(stopwords.words('english'))
+
+# Education Degrees
+EDUCATION = [
+            'BE','B.E.', 'B.E', 'BS', 'B.S', 
+            'ME', 'M.E', 'M.E.', 'MS', 'M.S', 
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH', 
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+        ]
+
+def extract_education(resume_text):
+    nlp_text = nlp(resume_text)
+
+    # Sentence Tokenizer
+    nlp_text = [sent.text.strip() for sent in nlp_text.sents]
+
+    edu = {}
+    # Extract education degree
+    for index, text in enumerate(nlp_text):
+        for tex in text.split():
+            # Replace all special symbols
+            tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+            if tex.upper() in EDUCATION and tex not in STOPWORDS:
+                edu[tex] = text + nlp_text[index + 1]
+
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(r'(((20|19)(\d{2})))'), edu[key])
+        if year:
+            education.append((key, ''.join(year[0])))
+        else:
+            education.append(key)
+    return education
+
+
+education = extract_education(text)
+print(education)
+extracted_text["Qualification"] = education 
+
+
+
+
+# import re
+# sub_patterns = ['[A-Z][a-z]* University','[A-Z][a-z]* Educational Institute',
+#                 'University of [A-Z][a-z]*',
+#                 'Ecole [A-Z][a-z]*']
+# pattern = '({})'.format('|'.join(sub_patterns))
+# matches = re.findall(pattern, text)
+
+# extracted_text["Institutes"] = matches
+# print(matches)
+
+print(extracted_text)
